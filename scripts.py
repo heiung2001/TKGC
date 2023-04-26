b7aefc9983400dc2c9983bf9c56d1074cb1c7685
heiung2001 <hieu.working2001@gmail.com>
2023-04-26 08:30:02 +0700

    move input to cuda

--- scripts.py
+++ scripts.py
@@ -2,10 +2,10 @@ import torch
 
 
 def shred_facts(facts):
-    heads = torch.tensor(facts[:, 0]).long()
-    rels = torch.tensor(facts[:, 1]).long()
-    tails = torch.tensor(facts[:, 2]).long()
-    years = torch.tensor(facts[:, 3]).float()
-    months = torch.tensor(facts[:, 4]).float()
-    days = torch.tensor(facts[:, 5]).float()
+    heads = torch.tensor(facts[:, 0]).long().cuda()
+    rels = torch.tensor(facts[:, 1]).long().cuda()
+    tails = torch.tensor(facts[:, 2]).long().cuda()
+    years = torch.tensor(facts[:, 3]).float().cuda()
+    months = torch.tensor(facts[:, 4]).float().cuda()
+    days = torch.tensor(facts[:, 5]).float().cuda()
     return heads, rels, tails, years, months, days
