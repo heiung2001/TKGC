7c1b67f02e3f88488732657aa20876113a57fc3f
heiung2001 <hieu.working2001@gmail.com>
2023-06-25 11:17:35 +0700

    add collate function

---
+++ utils/helper.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+
+def shred_facts(batch):
+    np_batch = np.array(batch)
+    heads = torch.tensor(np_batch[:, 0]).long()
+    rels = torch.tensor(np_batch[:, 1]).long()
+    tails = torch.tensor(np_batch[:, 2]).long()
+    years = torch.tensor(np_batch[:, 3]).float()
+    months = torch.tensor(np_batch[:, 4]).float()
+    days = torch.tensor(np_batch[:, 5]).float()
+    date_id = torch.tensor(np_batch[:, 6]).long()
+    return heads, rels, tails, years, months, days, date_id
